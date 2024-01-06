a15831eec13e5de894a997f6a53668c6468c72f8
csabakiss1 <155373943+csabakiss1@users.noreply.github.com>
2024-01-06 22:36:33 +0100

    Add files via upload

---
+++ emsckpi-api_json.py
@@ -0,0 +1,58 @@
+# 2024.01.05  12.00
+import pandas as pd 
+import json as js
+from sqlalchemy import create_engine
+from sqlalchemy.engine import URL
+from fastapi import FastAPI
+
+# MSSQL SQLAlchemy connection
+azure_sql_conn = URL.create("mssql+pyodbc", host="emsckpi-api.database.windows.net", port="1433", database="emsckpi-api",
+    query={"driver":"ODBC Driver 17 for SQL Server", "authentication":"ActiveDirectoryIntegrated", "Encrypt":"yes", "TrustServerCertificate": "yes"}) 
+
+engine = create_engine(azure_sql_conn)
+sqlalch_conn = engine.connect()
+
+safety_df = pd.read_sql("SELECT TOP 10 * FROM dbo.snowflake_table", sqlalch_conn)
+#print(safety_df.head)
+
+#sqlalch_conn.close()
+#engine.dispose()
+
+api_server = FastAPI()
+
+@api_server.get('/')
+def default_route():
+    return {"message":"serving data from snowflake_table"}
+
+@api_server.get('/level1')
+def query_data1():
+    safety_lev1_select = safety_df['EARNINGS_VIEW_LVL1'].unique().tolist()
+    level1_dict = {'EARNINGS_VIEW_LVL1':safety_lev1_select}
+    return js.loads(js.dumps(level1_dict)) 
+
+@api_server.get('/level2')
+def query_data2():
+    safety_lev2_select = safety_df['EARNINGS_VIEW_LVL2'].unique().tolist()
+    level2_dict = {'EARNINGS_VIEW_LVL2':safety_lev2_select}
+    return js.loads(js.dumps(level2_dict)) 
+
+@api_server.get('/level3')
+def query_data3():
+    safety_lev3_select = safety_df['EARNINGS_VIEW_LVL3'].unique().tolist()
+    level3_dict = {'EARNINGS_VIEW_LVL3':safety_lev3_select}
+    return js.loads(js.dumps(level3_dict)) 
+
+@api_server.get('/query_data')
+def query_data():
+    query1_data = pd.read_sql("SELECT TOP 50 * FROM dbo.snowflake_table", sqlalch_conn)
+    query1_json = query1_data.to_json(orient ='records')
+    return js.loads(query1_json)
+    #return js.loads(js.dumps(query1_json))
+
+import nest_asyncio
+import uvicorn
+if __name__ == "__main__":
+    nest_asyncio.apply()
+    uvicorn.run(api_server)
+
+
